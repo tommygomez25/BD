53412fd6fea9be8e811d9b9a76b5e01929f6a1d1
tommygomez25 <maysan54321@gmail.com>
2021-12-04 22:56:32 +0000

    small change to AlunoRealiza

--- Python scripts/AlunoRealiza.py
+++ Python scripts/AlunoRealiza.py
@@ -9,12 +9,12 @@ import pandas as pd
 import random
 
 S_N = ["S", "N"]
-sitFreqPossiveis = ["Admitido a exame", "Anulou a matrícula", "Excluído por faltas", "Reprovou por frequência"]
+sitFreqPossiveis = ["Admitido a exame", "Anulou a matrícula", "Excluído por faltas", "Reprovou frequência"]
 serInterno = ""
 classInt = 0
 notaExame = 0.0
 classFinal = 0
-paraAprov = "S"
+paraAprov = "N"
 paraMelhoria = ""
 provaIngresso = ""
 CFCEPE = ""
@@ -25,6 +25,10 @@ for i in range(1,68):
     if sitFreq == "Admitido a exame" :
         serInterno = "S"
         classInt = random.randrange(10,20)
+        if classInt < 14:
+            paraAprov = "S"
+        else:
+            paraAprov = "N"
         notaExame = round(random.uniform(0.0,20.0),1)
         classFinal = round(int(0.7*classInt + 0.3 * notaExame),0)
         paraMelhoria = random.choice(S_N)
@@ -33,6 +37,7 @@ for i in range(1,68):
     else:
         serInterno = "N"
         classInt = 0
+        paraAprov = "S"
         notaExame = round(random.uniform(0.0,20.0),1)
         classFinal = round(int(notaExame+0.5),0)
         paraMelhoria = random.choice(S_N)
