610eefa4315e361eabf8a50b64be5ac9c16e3de5
António Ferreira <80832984+up202004735@users.noreply.github.com>
2022-01-11 12:05:51 +0000

    Add files via upload

---
+++ queries/int4.sql
@@ -0,0 +1,10 @@
+.mode columns
+.width 8, 35, 8, 65
+.headers on
+.nullvalue NULL
+
+//Mostrar os alunos e o curso em que andam
+
+SELECT idAluno, Aluno.nome, Aluno.codCurso, Curso.nome 
+FROM Aluno JOIN Curso 
+USING (codCurso);
