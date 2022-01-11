fe04995eb9732874522f968dc1669d6c391c3605
FranciscoMaldonado0 <83646186+FranciscoMaldonado0@users.noreply.github.com>
2022-01-11 12:49:47 +0000

    Add files via upload

---
+++ queries/int6.sql
@@ -0,0 +1,13 @@
+.mode columns
+.width auto
+.headers on
+.nullvalue NULL
+
+//Valor absoluto da diferença entre as médias das Escolas Publicas e das Escolas Privadas
+
+SELECT ABS(mediaPublica.media - mediaPrivada.media) Difereça 
+FROM( 
+	SELECT AVG(notaExame) media FROM AlunoRealiza,OndeRealiza, Escola  WHERE AlunoRealiza.idAluno = OndeRealiza.idAluno AND Escola.tipo = 'PÚBLICO' AND OndeRealiza.idEscola = Escola.idEscola) AS mediaPublica, 
+( 
+	SELECT AVG(notaExame) media FROM AlunoRealiza,OndeRealiza, Escola  WHERE AlunoRealiza.idAluno = OndeRealiza.idAluno AND Escola.tipo = 'PRIVADO' AND OndeRealiza.idEscola = Escola.idEscola ) AS mediaPrivada;
+
