10c838c9e47ea14d844f82e44badc2f4ae34a7d0
António Ferreira <80832984+up202004735@users.noreply.github.com>
2022-01-13 11:33:26 +0000

    Add files via upload

---
+++ queries/int7.sql
@@ -0,0 +1,12 @@
+.mode columns
+.width 35 15
+.headers on
+.nullvalue NULL
+
+SELECT nome, notaExame
+FROM AlunoRealiza AR1, Aluno
+WHERE not exists ( 
+	SELECT * FROM AlunoRealiza AR2
+	WHERE AR2.notaExame > AR1.notaExame
+	)
+      AND AR1.idAluno = Aluno.idAluno;
