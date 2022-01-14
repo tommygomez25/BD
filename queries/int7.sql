f989aca86719bbce79ac0d3c82f4d06c52b5977e
Tomás Gomes <74729401+tommygomez25@users.noreply.github.com>
2022-01-14 12:09:52 +0000

    Update int7.sql

--- queries/int7.sql
+++ queries/int7.sql
@@ -3,12 +3,17 @@
 .headers on
 .nullvalue NULL
 
-//Mostrar o(s) aluno(s) com a nota máxima da base de dados
+--Mostrar o(s) aluno(s) com a nota máxima de cada escola
 
-SELECT nome, notaExame
-FROM AlunoRealiza AR1, Aluno
+create view if not exists ARExtensao as Select AlunoRealiza.idAluno,idEscola,notaExame from AlunoRealiza join OndeRealiza using(idAluno);
+
+SELECT Aluno.nome as nomeAluno, notaExame as melhorNota, Escola.nome as nomeEscola
+FROM ARExtensao AR1, Aluno, OndeRealiza join Escola using (idEscola)
 WHERE not exists ( 
-	SELECT * FROM AlunoRealiza AR2
+	SELECT * FROM ARExtensao AR2
 	WHERE AR2.notaExame > AR1.notaExame
+	AND AR2.idEscola = AR1.idEscola
 	)
-      AND AR1.idAluno = Aluno.idAluno;
+      AND AR1.idAluno = Aluno.idAluno AND OndeRealiza.idAluno = Aluno.idAluno
+GROUP BY OndeRealiza.idEscola
+ORDER BY notaExame;
