31fc875ad0572328b62037c35a43f67f97093b97
António Ferreira <80832984+up202004735@users.noreply.github.com>
2022-01-24 11:59:24 +0000

    Delete queries directory

--- queries/gatilho1_remove.sql
+++
@@ -1,9 +0,0 @@
-.mode columns
-.header on
-.nullvalue NULL
-
-PRAGMA foreign_keys = ON;
-
-DROP TRIGGER IF EXISTS InsertNovoAlunoExamePT;
-DROP TRIGGER IF EXISTS InsertNovoAlunoExameMAT;
-DROP TRIGGER IF EXISTS InsertNovoAlunoExameFQ;
--- queries/gatilho3_remove.sql
+++
@@ -1,9 +0,0 @@
-.mode columns
-.header on
-.nullvalue NULL
-
-PRAGMA foreign_keys = ON;
-
-DROP TRIGGER InsertOnView1;
-DROP TRIGGER InsertOnView2;
-DROP VIEW ExamesDisciplinas;
