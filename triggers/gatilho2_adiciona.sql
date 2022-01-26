9a7f061feb8d1afb764701ad1314d7b4320caf26
António Ferreira <80832984+up202004735@users.noreply.github.com>
2022-01-26 11:30:19 +0000

    Update gatilho2_adiciona.sql

--- triggers/gatilho2_adiciona.sql
+++ triggers/gatilho2_adiciona.sql
@@ -12,7 +12,7 @@ AFTER INSERT ON AlunoRealiza
 FOR EACH ROW
 WHEN( (SELECT fase FROM Exame where idExame = NEW.idExame) = 1 AND NEW.paraAprov = 'S' AND NEW.notaExame < 9.5)
 BEGIN
-INSERT INTO AlunoRealiza VALUES(NEW.idAluno,(SELECT idExame FROM Exame E1 WHERE E1.fase = 2 AND ((E1.anoLetivo, E1.codExame) IN (SELECT anoLetivo, codExame FROM Exame E2 WHERE NEW.idExame = E2.idExame))) , NEW.sitFrequencia, NEW.serInterno, 0.0, NEW.paraAprov, NEW.paraMelhoria, NEW.ProvaIngresso, NEW.CFCEPE); 
+INSERT INTO AlunoRealiza VALUES(NEW.idAluno,(SELECT idExame FROM Exame E1 WHERE E1.fase = 2 AND ((E1.anoLetivo, E1.codExame) IN (SELECT anoLetivo, codExame FROM Exame E2 WHERE NEW.idExame = E2.idExame))) , NEW.sitFrequencia, NEW.serInterno, NULL, NEW.paraAprov, NEW.paraMelhoria, NEW.ProvaIngresso, NEW.CFCEPE); 
 END;
 
 --Quando se faz o update dos dados de um aluno para que ele tenha feito um exame da 1ª fase, para aprovação, 
@@ -23,5 +23,5 @@ AFTER UPDATE ON AlunoRealiza
 FOR EACH ROW
 WHEN( (SELECT fase FROM Exame where idExame = NEW.idExame) = 1 AND NEW.paraAprov = 'S' AND NEW.notaExame < 9.5 )
 BEGIN
-INSERT INTO AlunoRealiza VALUES(NEW.idAluno, (SELECT idExame FROM Exame E1 WHERE E1.fase = 2 AND ((E1.anoLetivo, E1.codExame) IN (SELECT anoLetivo, codExame FROM Exame E2 WHERE NEW.idExame = E2.idExame))), NEW.sitFrequencia, NEW.serInterno, 0.0, NEW.paraAprov, NEW.paraMelhoria, NEW.ProvaIngresso, NEW.CFCEPE); 
+INSERT INTO AlunoRealiza VALUES(NEW.idAluno, (SELECT idExame FROM Exame E1 WHERE E1.fase = 2 AND ((E1.anoLetivo, E1.codExame) IN (SELECT anoLetivo, codExame FROM Exame E2 WHERE NEW.idExame = E2.idExame))), NEW.sitFrequencia, NEW.serInterno, NULL, NEW.paraAprov, NEW.paraMelhoria, NEW.ProvaIngresso, NEW.CFCEPE); 
 END;
